a9e23f67a676214ae0df9401948ce9f1fc16d601
Vishwanath-C <vishwanath.kardvg@gmail.com>
2025-10-04 02:42:13 +0530

    update

--- backend/src/main/java/com/example/MovieTicketBooking/MovieTicketBookingApplication.java
+++ backend/src/main/java/com/example/MovieTicketBooking/MovieTicketBookingApplication.java
@@ -2,8 +2,10 @@ package com.example.MovieTicketBooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.example.MovieTicketBooking.model")
 public class MovieTicketBookingApplication {
 
 	public static void main(String[] args) {
